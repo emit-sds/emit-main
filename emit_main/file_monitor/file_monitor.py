30c72bd7155f1e98b433a1675a2ec86057be7e08
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2020-10-22 14:44:57 -0700

    Add new run() method to file monitor that groups ingested files by time range and only executes on largest file

--- emit_main/file_monitor/file_monitor.py
+++ emit_main/file_monitor/file_monitor.py
@@ -4,13 +4,13 @@ This code contains the FileMonitor class that watches folders to trigger activit
 Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
 """
 
+import glob
 import json
 import logging
 import os
-import subprocess
 import time
 
-from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 
 logger = logging.getLogger("emit-main")
@@ -32,33 +32,62 @@ class FileMonitor:
         # Build path for ingest folder
         self.ingest_dir = os.path.join(self.local_store_dir, self.instrument, self.environment, "ingest")
 
-        self.observer = Observer()
+        self.observer = PollingObserver(timeout=15)
 
     def run(self):
-        event_handler = Handler()
+        """
+        Look at all files in ingest folder and run largest file for each time range. Archive duplicates.
+        """
+
+        ingest_files = [os.path.basename(path) for path in glob.glob(os.path.join(self.ingest_dir, "*hsc.bin"))]
+        prefix_hash = {}
+        # Group files by prefix to find matching time ranges
+        for file in ingest_files:
+            file_prefix = file[:35]
+            if file_prefix not in prefix_hash.keys():
+                prefix_hash[file_prefix] = {file: os.path.getsize(os.path.join(self.ingest_dir, file))}
+            else:
+                prefix_hash[file_prefix].update({file: os.path.getsize(os.path.join(self.ingest_dir, file))})
+        for group in prefix_hash.values():
+            if len(group.items()) == 1:
+                for file in group.keys():
+                    # Run workflow
+                    logger.info("Running workflow on %s" % file)
+            else:
+                max_file = [key for (key, value) in group.items() if value == max(group.values())][0]
+                for file in group.keys():
+                    if file == max_file:
+                        logger.info("Running workflow on max_file %s" % file)
+                    else:
+                        logger.info("Archiving duplicate file %s" % file)
+
+        return
+
+    def run_observer(self):
+        event_handler = IngestHandler()
         self.observer.schedule(event_handler, self.ingest_dir, recursive=True)
         self.observer.start()
         logger.info("Observer started.")
         try:
             while True:
-                time.sleep(5)
-        except RuntimeError:
+                time.sleep(1)
+        except KeyboardInterrupt:
             self.observer.stop()
             logger.info("Observer Stopped")
 
         self.observer.join()
 
 
-class Handler(FileSystemEventHandler):
+class IngestHandler(FileSystemEventHandler):
 
     @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
+    def on_created(event):
+        logger.info("Watchdog received created event - % s." % event.src_path)
 
-        elif event.event_type == 'created':
-            # Event is created, you can process it now
-            logger.info("Watchdog received created event - % s." % event.src_path)
-        elif event.event_type == 'modified':
-            # Event is modified, you can process it now
-            logger.info("Watchdog received modified event - % s." % event.src_path)
+
+    @staticmethod
+    def on_modified(event):
+        logger.info("Watchdog received modified event - % s." % event.src_path)
+        # TODO: Move this code to on_created section
+        # Check to see if there's another file with same start/stop.  If so, archive smaller file and process
+        # larger one
