622c77cf2d3e39bb948f52b8272783f16dfe0ed6
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2020-12-03 15:46:14 -0800

    Add run_workflow commands to ingest files.

--- emit_main/file_monitor/file_monitor.py
+++ emit_main/file_monitor/file_monitor.py
@@ -8,10 +8,8 @@ import glob
 import json
 import logging
 import os
-import time
 
-from watchdog.observers.polling import PollingObserver
-from watchdog.events import FileSystemEventHandler
+from emit_main.workflow.workflow_manager import WorkflowManager
 
 logger = logging.getLogger("emit-main")
 
@@ -23,16 +21,16 @@ class FileMonitor:
         :param config_path: Path to config file containing environment settings
         """
 
+        self.config_path = config_path
         # Read config file for environment specific paths
         with open(config_path, "r") as f:
             config = json.load(f)
             self.__dict__.update(config["general_config"])
             self.__dict__.update(config["filesystem_config"])
+            self.__dict__.update(config["build_config"])
 
         # Build path for ingest folder
         self.ingest_dir = os.path.join(self.local_store_dir, self.instrument, self.environment, "ingest")
-
-        self.observer = PollingObserver(timeout=15)
 
     def ingest_files(self):
         """
@@ -41,11 +39,11 @@ class FileMonitor:
         ingest_files = [os.path.basename(path) for path in glob.glob(os.path.join(self.ingest_dir, "*hsc.bin"))]
         self._ingest_file_list(ingest_files)
 
-    def ingest_files_by_time_range(self, start_time_str, stop_time_str):
+    def ingest_files_by_time_range(self, start_time, stop_time):
         """
         Only process files in ingest folder within a specific datetime range
-        :param start_time_str: Start time in format YYMMDDhhmmss
-        :param stop_time_str: Stop time in format YYMMDDhhmmss
+        :param start_time: Start time in format YYMMDDhhmmss
+        :param stop_time: Stop time in format YYMMDDhhmmss
         """
         matching_files = []
         ingest_files = [os.path.basename(path) for path in glob.glob(os.path.join(self.ingest_dir, "*hsc.bin"))]
@@ -53,7 +51,7 @@ class FileMonitor:
             tokens = file.split("_")
             file_start = tokens[2]
             file_stop = tokens[3]
-            if file_start >= start_time_str and file_stop <= stop_time_str:
+            if file_start >= start_time and file_stop <= stop_time:
                 matching_files.append(file)
         self._ingest_file_list(matching_files)
 
@@ -66,44 +64,26 @@ class FileMonitor:
                 prefix_hash[file_prefix] = {file: os.path.getsize(os.path.join(self.ingest_dir, file))}
             else:
                 prefix_hash[file_prefix].update({file: os.path.getsize(os.path.join(self.ingest_dir, file))})
+        # Get run command path
+        wm = WorkflowManager(self.config_path)
+#        wm.build_runtime_environment()
+        pge = wm.pges["emit-main"]
+        run_workflow_exe = os.path.join(pge.repo_dir, "emit_main", "run_workflow.py")
+        # TODO: Change "cmd" below based on APID
         for group in prefix_hash.values():
             if len(group.items()) == 1:
                 for file in group.keys():
                     # Run workflow
-                    logger.info("Running workflow on %s" % file)
+                    path = os.path.join(self.ingest_dir, file)
+                    logger.info("Running workflow on %s" % path)
+                    cmd = ["python", run_workflow_exe, "-c", self.config_path, "-s", path, "-p", "l1aeng"]
+                    pge.run(cmd)
             else:
                 max_file = [key for (key, value) in group.items() if value == max(group.values())][0]
                 for file in group.keys():
                     if file == max_file:
                         logger.info("Running workflow on max_file %s" % file)
+                        cmd = ["python", run_workflow_exe, "-c", self.config_path, "-s", file, "-p", "l1aeng"]
+                        pge.run(cmd)
                     else:
-                        logger.info("Archiving duplicate file %s" % file)
-
-    def run_observer(self):
-        event_handler = IngestHandler()
-        self.observer.schedule(event_handler, self.ingest_dir, recursive=True)
-        self.observer.start()
-        logger.info("Observer started.")
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            self.observer.stop()
-            logger.info("Observer Stopped")
-
-        self.observer.join()
-
-
-class IngestHandler(FileSystemEventHandler):
-
-    @staticmethod
-    def on_created(event):
-        logger.info("Watchdog received created event - % s." % event.src_path)
-
-
-    @staticmethod
-    def on_modified(event):
-        logger.info("Watchdog received modified event - % s." % event.src_path)
-        # TODO: Move this code to on_created section
-        # Check to see if there's another file with same start/stop.  If so, archive smaller file and process
-        # larger one
+                        logger.info("Archiving duplicate smaller file %s" % file)
