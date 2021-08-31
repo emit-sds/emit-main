d190b26d986f76aa8b9f399b8b5024a98c1f596e
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-08-31 14:35:30 -0700

    Update version to 0.2 for SDS release 2.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emit_main",
-    version="0.1",
+    version="0.2",
     author="Winston Olson-Duvall",
     author_email="winston.olson-duvall@jpl.nasa.gov",
     description="Workflow and file management code for EMIT data processing",
