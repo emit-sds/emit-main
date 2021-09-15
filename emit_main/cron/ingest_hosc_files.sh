f251034386ca0fa758591c1150d6c5f0cf105357
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-09-15 13:12:58 -0700

    Fix script to work with dev and ops environments

--- emit_main/cron/ingest_hosc_files.sh
+++ emit_main/cron/ingest_hosc_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# $1 is the environment, either "test" or "ops"
+# $1 is the environment, either "dev", "test", or "ops"
 # $2 is the conda environment
 
 T=$(date)
 
 echo
-echo "$T: Executing ingest_hosc_files.sh cron job with '$1' argument"
+echo "$T: Executing ingest_hosc_files.sh cron job with '$1' environment and '$2' conda environment"
 echo
 
 source /shared/anaconda3/etc/profile.d/conda.sh
 conda activate $2
 
 cd /store/emit/$1/repos/emit-main/emit_main
-python run_file_monitor.py -c config/test_sds_config.json
+python run_file_monitor.py -c config/${1}_sds_config.json
