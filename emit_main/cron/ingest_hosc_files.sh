b83551e5dcc3ef3fc482fa5510aa1eb010946889
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-09-15 14:01:18 -0700

    Add optional args to ingest script

--- emit_main/cron/ingest_hosc_files.sh
+++ emit_main/cron/ingest_hosc_files.sh
@@ -2,15 +2,17 @@
 
 # $1 is the environment, either "dev", "test", or "ops"
 # $2 is the conda environment
+# $3 Optional args to pass into run_file_monitor (like --test_mode)
 
 T=$(date)
 
 echo
-echo "$T: Executing ingest_hosc_files.sh cron job with '$1' environment and '$2' conda environment"
+echo "$T: Executing ingest_hosc_files.sh cron job with '$1' environment, '$2' conda environment, and '$3' optional args"
 echo
 
 source /shared/anaconda3/etc/profile.d/conda.sh
 conda activate $2
 
 cd /store/emit/$1/repos/emit-main/emit_main
-python run_file_monitor.py -c config/${1}_sds_config.json
+
+python run_file_monitor.py -c config/${1}_sds_config.json $3
