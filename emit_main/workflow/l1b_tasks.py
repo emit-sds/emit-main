c97f229da9c176520abeb632a1d4dea25361c080
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-01-14 10:16:18 -0800

    Only make remote directory and change ownership if it doesn't exist.

--- emit_main/workflow/l1b_tasks.py
+++ emit_main/workflow/l1b_tasks.py
@@ -352,14 +352,11 @@ class L1BDeliver(SlurmJobTask):
         log_file_arg = f"--log-file={os.path.join(self.tmp_dir, 'rsync.log')}"
         target = f"{wm.config['daac_server_internal']}:{acq.daac_staging_dir}/"
         group = f"emit-{wm.config['environment']}" if wm.config["environment"] in ("test", "ops") else "emit-dev"
-        cmd_make_target = ["ssh", wm.config["daac_server_internal"], "\"mkdir", "-p", acq.daac_staging_dir, "&&",
-                           "chgrp", group, f"{acq.daac_staging_dir}\""]
-        try:
-            pge.run(cmd_make_target, tmp_dir=self.tmp_dir)
-        except RuntimeError:
-            wm.print(__name__, f"Ran into error while trying to make dir and change group permissions on "
-                     f"{acq.daac_staging_dir}. Assuming that the remote directory is owned by another user and "
-                     f"continuing.")
+        # This command only makes the directory and changes ownership if the directory doesn't exist
+        cmd_make_target = ["ssh", wm.config["daac_server_internal"], "\"[", "!", "-d", f"'{acq.daac_staging_dir}'",
+                           "]", "&&", "mkdir", acq.daac_staging_dir, "&&", "chgrp", group, f"{acq.daac_staging_dir}\""]
+        pge.run(cmd_make_target, tmp_dir=self.tmp_dir)
+
         cmd_rsync_nc = ["rsync", "-azv", partial_dir_arg, log_file_arg, daac_nc_path, target]
         cmd_rsync_json = ["rsync", "-azv", partial_dir_arg, log_file_arg, daac_ummg_json_path, target]
         pge.run(cmd_rsync_nc, tmp_dir=self.tmp_dir)
