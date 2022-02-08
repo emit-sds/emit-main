b6d434a228f7fa8565b456548813a7c4e27b9337
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-02-08 11:29:25 -0800

    Fix pep 8 issues.

--- emit_main/workflow/l1b_tasks.py
+++ emit_main/workflow/l1b_tasks.py
@@ -397,20 +397,19 @@ class L1BGeolocate(SlurmJobTask):
         # Update attitude/ephemeris netcdf metadata before copy
         ae_nc = netCDF4.Dataset(tmp_corr_att_eph_path, 'r+')
         daac_converter.makeGlobalAttrBase(ae_nc)
-        ae_nc.title = "EMIT L1B Corrected Spacecraft Attitude and Ephemeris (ATT), V001" 
+        ae_nc.title = "EMIT L1B Corrected Spacecraft Attitude and Ephemeris (ATT), V001"
         ae_nc.summary = ae_nc.summary + \
-        f"\\n\\nThis collection contains L1B Corrected Spacecraft Attitude and Ephemeris (ATT).\
-        ATT contains the uncorrected Broadcast Ancillary Data (BAD) ephemeris and attitude quaternions \
-        from the ISS, and the data after correction by the geolocation process. \
-        This product is generated at the orbit level.\
-        "
+            f"\\n\\nThis collection contains L1B Corrected Spacecraft Attitude and Ephemeris (ATT).\
+            ATT contains the uncorrected Broadcast Ancillary Data (BAD) ephemeris and attitude quaternions \
+            from the ISS, and the data after correction by the geolocation process. \
+            This product is generated at the orbit level.\
+            "
         ae_nc.product_version = wm.config["extended_build_num"]
         ae_nc.time_coverage_start = orbit.start_time.strftime("%Y-%m-%dT%H:%M:%S%z")
         ae_nc.time_coverage_end = orbit.stop_time.strftime("%Y-%m-%dT%H:%M:%S%z")
 
         ae_nc.sync()
         ae_nc.close()
-
 
         wm.copy(tmp_corr_att_eph_path, orbit.corr_att_eph_path)
 
