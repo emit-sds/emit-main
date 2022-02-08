098e9022158002d69f17f3385a4556fa7b4a45ca
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-02-08 12:43:51 -0800

    Add julia_exe to config and pass in /shared/julia-1.6.5/bin/julia. Update L3Task to use.

--- emit_main/workflow/l3_tasks.py
+++ emit_main/workflow/l3_tasks.py
@@ -59,13 +59,14 @@ class L3Unmix(SlurmJobTask):
         pge = wm.pges["emit-sds-l3"]
 
         # Build PGE commands for run_tetracorder_pge.sh
+        julia_exe = wm.config["julia_exe"]
         unmix_exe = os.path.join(pge.repo_dir, "unmix.jl")
         endmember_path = os.path.join(pge.repo_dir, "data", "endmember_library.csv")
         endmember_key = "Class"
         log_path = acq.cover_img_path.replace(".img", "_pge.log")
         output_base = os.path.join(self.local_tmp_dir, "unmixing_output")
 
-        cmd_unmix = ['julia', '-p', str(self.n_cores), unmix_exe, acq.rfl_img_path, endmember_path, endmember_key, output_base, "--normalization",
+        cmd_unmix = [julia_exe, '-p', str(self.n_cores), unmix_exe, acq.rfl_img_path, endmember_path, endmember_key, output_base, "--normalization",
                      "brightness", "--n_mc", "100", "--reflectance_uncertainty_file", acq.uncert_img_path,
                      "--spectral_starting_column", "2", "--num_endmembers", "-1", "--log_file", log_path]
 
