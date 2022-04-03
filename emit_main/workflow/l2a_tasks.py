c658e786d5202753853ad1cd238686358d1d2829
pgbrodrick <phil.brodrick@gmail.com>
2022-04-02 18:38:37 -0700

    change masks to run in parallel from main

--- emit_main/workflow/l2a_tasks.py
+++ emit_main/workflow/l2a_tasks.py
@@ -181,6 +181,8 @@ class L2AMask(SlurmJobTask):
     level = luigi.Parameter()
     partition = luigi.Parameter()
 
+    n_cores = 40
+    memory = 180000
     task_namespace = "emit"
 
     def requires(self):
@@ -223,7 +225,7 @@ class L2AMask(SlurmJobTask):
         }
 
         cmd = ["python", make_masks_exe, acq.rdn_img_path, acq.loc_img_path, acq.lbl_img_path, acq.statesubs_img_path,
-               solar_irradiance_path, tmp_mask_path]
+               solar_irradiance_path, tmp_mask_path, "--n_cores", self.n_cores]
         pge.run(cmd, tmp_dir=self.tmp_dir)
 
         # Copy mask files to l2a dir
