064cacc480367be03be810686c58683e17da1842
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2024-04-11 10:18:34 -0700

    Add scene metrics and option to export

--- emit_main/cron/compile_metrics.py
+++ emit_main/cron/compile_metrics.py
@@ -7,8 +7,76 @@ Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
 import argparse
 import datetime as dt
 import glob
+import json
 import os
 import sys
+
+import pandas as pd
+
+import spectral.io.envi as envi
+
+from emit_main.database.database_manager import DatabaseManager
+
+
+def update_collection(db_collection, query, data_frame):
+    # Check if documents have changes before updating
+    utc_now = dt.datetime.now(tz=dt.timezone.utc)
+    result = db_collection.find_one(query)
+
+    if result is None:
+        data_frame["last_modified"] = utc_now
+        db_collection.insert_one(data_frame)
+        print(f"Inserted DB with {data_frame}")
+        return
+
+    # If result is not none then compare new values with existing and only update if different
+    if "_id" in result:
+        del result["_id"]
+    if "last_modified" in result:
+        del result["last_modified"]
+
+    needs_update = False
+    for k,v in data_frame.items():
+        if k not in result or result[k] != data_frame[k]:
+            needs_update = True
+
+    if needs_update:
+        data_frame["last_modified"] = utc_now
+        db_collection.update_one(query, {"$set": data_frame})
+        print(f"Updated DB with {data_frame}")
+    else:
+        print("DB already contains matching document, so no update.")
+
+
+def export_apid(apid, dm, query, out_dir, start, stop):
+    if apid == "1674":
+        apid_coll = dm.db.trending_1674
+    elif apid == "1675":
+        apid_coll = dm.db.trending_1675
+    elif apid == "1676":
+        apid_coll = dm.db.trending_1676
+    results = list(apid_coll.find(query).sort("timestamp", 1))
+    if len(results) > 0:
+        utc_now_str = dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        apid_outfile = f"{out_dir}/metrics_{apid}_{start}_{stop}.csv"
+        df = pd.DataFrame(results)
+        df = df.drop(["_id"], axis=1)
+        df["timestamp"] = df["timestamp"].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        df["last_modified"] = df["last_modified"].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        df.to_csv(apid_outfile, sep=",", index=False)
+
+
+def export_scene(dm, query, out_dir, start, stop):
+    trending_acqs_coll = dm.db.trending_acquisitions
+    results = list(trending_acqs_coll.find(query).sort("timestamp", 1))
+    if len(results) > 0:
+        utc_now_str = dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        outfile = f"{out_dir}/metrics_scenes_{start}_{stop}.csv"
+        df = pd.DataFrame(results)
+        df = df.drop(["_id"], axis=1)
+        df["timestamp"] = df["timestamp"].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        df["last_modified"] = df["last_modified"].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        df.to_csv(outfile, sep=",", index=False)
 
 
 def main():
@@ -16,7 +84,9 @@ def main():
     parser = argparse.ArgumentParser(description="Compile metrics for tracking")
     parser.add_argument("-e", "--env", default="ops", help="Where to run the report")
     parser.add_argument("-d", "--dates", default="20230801,20230805", help="Comma separated dates (YYYYMMDD,YYYYMMDD)")
-    parser.add_argument("-o", "--output", help="Output report path (use .csv extension)")
+    parser.add_argument("--tracking_json", default="/scratch/brodrick/emit/emit-visuals/track_coverage.json",
+                        help="JSON containing scene metrics")
+    parser.add_argument("--export_to_dir", default=None)
     args = parser.parse_args()
 
     env = args.env
@@ -24,25 +94,39 @@ def main():
     if args.dates is not None:
         start, stop = args.dates.split(",")
 
-    if args.output is None:
-        args.output = f"/store/emit/{env}/reports/trending/metrics.csv"
+    start_date = dt.datetime.strptime(start, "%Y%m%d")
+    stop_date = dt.datetime.strptime(stop, "%Y%m%d")
 
-    outfile = open(args.output, "w")
-    outfile.write("TIMESTAMP,APID,PACKETS,PSC_GAPS,MISSING_PACKETS,DUPLICATE_PACKETS\n")
+    config_path = f"/store/emit/{args.env}/repos/emit-main/emit_main/config/{args.env}_sds_config.json"
+    print(f"Using config_path {config_path}")
+    dm = DatabaseManager(config_path)
 
+    # If exporting, just export CSVs based on date range
+    if args.export_to_dir:
+        print(f"Exporting to directory {args.export_to_dir}")
+        if not os.path.exists(args.export_to_dir):
+            os.makedirs(args.export_to_dir)
+        query = {
+            "timestamp": {"$gte": start_date, "$lt": stop_date}
+        }
+
+        for apid in ["1674", "1675", "1676"]:
+            print(f"Exporting apid {apid} to CSV")
+            export_apid(apid, dm, query, args.export_to_dir, start, stop)
+
+        print("Exporting scene data to CSV")
+        export_scene(dm, query, args.export_to_dir, start, stop)
+        sys.exit()
+
+    # Collect metrics on APID streams
     for apid in ["1674", "1675", "1676"]:
-        # Set up outfile based on APID
-        apid_outfile = args.output.replace(".csv", f"_{apid}.csv")
-        outfile = open(apid_outfile, "w")
-        outfile.write(f"TIMESTAMP,APID,{apid}_PACKETS,{apid}_PSC_GAPS,{apid}_MISSING_PACKETS,{apid}_DUPLICATE_PACKETS,"
-                      f"{apid}_TIMING_ERRORS\n")
-
+        print(f"Collecting metrics on apid {apid}")
         date_dirs = glob.glob(f"/store/emit/{env}/data/streams/{apid}/*")
         start_dir = f"/store/emit/{env}/data/streams/{apid}/{start}"
         stop_dir = f"/store/emit/{env}/data/streams/{apid}/{stop}"
-        date_dirs = [dir for dir in date_dirs if start_dir <= dir <= stop_dir]
+        date_dirs = [dir for dir in date_dirs if start_dir <= dir < stop_dir]
         date_dirs.sort()
-        # print(f"The filtered list of dirs to check is {date_dirs}")
+        print(f"The filtered list of stream dirs to check is {date_dirs}")
 
         for dir in date_dirs:
             l0_reports = glob.glob(f"{dir}/l0/*report.txt")
@@ -62,11 +146,201 @@ def main():
                             timing_errors = int(line.rstrip("\n").split(" ")[-1])
                 timestamp_str = os.path.basename(report).split("_")[2]
                 timestamp = dt.datetime.strptime(timestamp_str, "%Y%m%dt%H%M%S")
-                timestamp_utc = timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
-                outfile.write(",".join([timestamp_utc, apid, str(packet_count), str(psc_gaps), str(missing_packets),
-                                        str(duplicate_packets), str(timing_errors)]) + "\n")
+                # timestamp_utc = timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+                df = {
+                    "timestamp": timestamp,
+                    f"{apid}_packets": packet_count,
+                    f"{apid}_psc_gaps": psc_gaps,
+                    f"{apid}_missing_packets": missing_packets,
+                    f"{apid}_duplicate_packets": duplicate_packets,
+                    f"{apid}_timing_errors": timing_errors
+                }
 
-    outfile.close()
+                if apid == "1674":
+                    apid_coll = dm.db.trending_1674
+                elif apid == "1675":
+                    apid_coll = dm.db.trending_1675
+                elif apid == "1676":
+                    apid_coll = dm.db.trending_1676
+                query = {"timestamp": timestamp}
+                # set_value = {"$set": df}
+                # print(f"Updating trending_{apid} with {df}")
+                update_collection(apid_coll, query, df)
+                # apid_coll.update_one(query, set_value, upsert=True)
+
+    # Collect metrics on acquisitions/scenes
+    print(f"Collecting metrics on acquisitions/scenes")
+    date_dirs = glob.glob(f"/store/emit/{env}/data/acquisitions/*")
+    start_dir = f"/store/emit/{env}/data/acquisitions/{start}"
+    stop_dir = f"/store/emit/{env}/data/acquisitions/{stop}"
+    date_dirs = [dir for dir in date_dirs if start_dir <= dir < stop_dir]
+    date_dirs.sort()
+    print(f"The filtered list of acquisition dirs to check is {date_dirs}")
+
+    # First, get data from acquisition directories
+    print("Checking acquisition directories")
+    for dir in date_dirs:
+        acqs = [os.path.basename(a) for a in glob.glob(f"{dir}/*")]
+        # print(f"Found acqs: {acqs}")
+        for acq in acqs:
+            timestamp_str = os.path.basename(acq).split("_")[0][4:]
+            timestamp = dt.datetime.strptime(timestamp_str, "%Y%m%dt%H%M%S")
+            df = {
+                "timestamp": timestamp,
+                "acquisition_id": acq
+            }
+            raw_img_files = glob.glob(f"{dir}/{acq}/l1a/{acq}*_raw_*img")
+            reassembly_reports = glob.glob(f"{dir}/{acq}/l1a/{acq}*_raw_*report.txt")
+            rdn_img_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_rdn_*img")
+            rdn_hdr_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_rdn_*hdr")
+            rdn_nc_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_rdn_*nc")
+            glt_img_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_glt_*img")
+            loc_img_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_loc_*img")
+            obs_img_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_obs_*img")
+            obs_nc_files = glob.glob(f"{dir}/{acq}/l1b/{acq}*_obs_*nc")
+            rfl_img_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_rfl_*img")
+            rfl_nc_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_rfl_*nc")
+            rflunc_img_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_rfluncert_*img")
+            rflunc_nc_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_rfluncert_*nc")
+            mask_img_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_mask_*img")
+            mask_nc_files = glob.glob(f"{dir}/{acq}/l2a/{acq}*_mask_*nc")
+            min_img_files = glob.glob(f"{dir}/{acq}/l2b/{acq}*_abun_*img")
+            min_nc_files = glob.glob(f"{dir}/{acq}/l2b/{acq}*_abun_*nc")
+            minunc_img_files = glob.glob(f"{dir}/{acq}/l2b/{acq}*_abununcert_*img")
+            minunc_nc_files = glob.glob(f"{dir}/{acq}/l2b/{acq}*_abununcert_*nc")
+            df["l1a_raw_exists"], df["l1b_radiance_exists"] = False, False
+            df["l2a_reflectance_exists"], df["l2b_mineral_id_exists"] = False, False
+
+            if len(raw_img_files) > 0:
+                df["raw_img_size_bytes"] = os.path.getsize(raw_img_files[0])
+                df["l1a_raw_exists"] = True
+            if len(rdn_img_files) > 0:
+                df["rdn_img_size_bytes"] = os.path.getsize(rdn_img_files[0])
+                df["l1b_radiance_exists"] = True
+            if len(rdn_nc_files) > 0:
+                df["rdn_nc_size_bytes"] = os.path.getsize(rdn_nc_files[0])
+            if len(glt_img_files) > 0:
+                df["glt_img_size_bytes"] = os.path.getsize(glt_img_files[0])
+            if len(loc_img_files) > 0:
+                df["loc_img_size_bytes"] = os.path.getsize(loc_img_files[0])
+            if len(obs_img_files) > 0:
+                df["obs_img_size_bytes"] = os.path.getsize(obs_img_files[0])
+            if len(obs_nc_files) > 0:
+                df["obs_nc_size_bytes"] = os.path.getsize(obs_nc_files[0])
+            if len(rfl_img_files) > 0:
+                df["rfl_img_size_bytes"] = os.path.getsize(rfl_img_files[0])
+                df["l2a_reflectance_exists"] = True
+            if len(rfl_nc_files) > 0:
+                df["rfl_nc_size_bytes"] = os.path.getsize(rfl_nc_files[0])
+            if len(rflunc_img_files) > 0:
+                df["rflunc_img_size_bytes"] = os.path.getsize(rflunc_img_files[0])
+            if len(rflunc_nc_files) > 0:
+                df["rflunc_nc_size_bytes"] = os.path.getsize(rflunc_nc_files[0])
+            if len(mask_img_files) > 0:
+                df["mask_img_size_bytes"] = os.path.getsize(mask_img_files[0])
+            if len(mask_nc_files) > 0:
+                df["mask_nc_size_bytes"] = os.path.getsize(mask_nc_files[0])
+            if len(min_img_files) > 0:
+                df["min_img_size_bytes"] = os.path.getsize(min_img_files[0])
+                df["l2b_mineral_id_exists"] = True
+            if len(min_nc_files) > 0:
+                df["min_nc_size_bytes"] = os.path.getsize(min_nc_files[0])
+            if len(minunc_img_files) > 0:
+                df["minunc_img_size_bytes"] = os.path.getsize(minunc_img_files[0])
+            if len(minunc_nc_files) > 0:
+                df["minunc_nc_size_bytes"] = os.path.getsize(minunc_nc_files[0])
+
+            # Get reassembly report info
+            if len(reassembly_reports) > 0:
+                num_lines, corrupt_lines, cloudy_frames = 0, 0, 0
+                lines_per_frame = 32  # default it but also get it below
+                missing_frames, corrupt_frames, decompression_errors = 0, 0, 0
+                submode = ""
+                with open(reassembly_reports[0], "r") as f:
+                    for line in f.readlines():
+                        if "Submode:" in line:
+                            submode = line.rstrip("\n").split(" ")[-1]
+                        if "Number of lines:" in line:
+                            num_lines = int(line.rstrip("\n").split(" ")[-1])
+                        if "Total corrupt lines" in line:
+                            corrupt_lines = int(line.rstrip("\n").split(" ")[-1])
+                        if "Number of lines per frame" in line:
+                            lines_per_frame = int(line.rstrip("\n").split(" ")[-1])
+                        if "Total cloudy frames" in line:
+                            cloudy_frames = int(line.rstrip("\n").split(" ")[-1])
+                        if "Total missing frames" in line:
+                            missing_frames = int(line.rstrip("\n").split(" ")[-1])
+                        if "Total corrupt frames" in line:
+                            corrupt_frames = int(line.rstrip("\n").split(" ")[-1])
+                        if "Total decompression errors" in line:
+                            decompression_errors = int(line.rstrip("\n").split(" ")[-1])
+
+                df.update({
+                    "submode": submode,
+                    "lines": num_lines,
+                    "corrupt_lines": corrupt_lines,
+                    "frames": num_lines // lines_per_frame,
+                    "cloudy_frames": cloudy_frames,
+                    "missing_frames": missing_frames,
+                    "corrupt_frames": corrupt_frames,
+                    "decompression_errors": decompression_errors
+                })
+
+            # Get masked pixel noise from radiance header
+            if len(rdn_hdr_files) > 0:
+                hdr = envi.read_envi_header(rdn_hdr_files[0])
+                if "masked pixel noise" in hdr:
+                    df["masked_pixel_noise"] = float(hdr["masked pixel noise"])
+
+            # Update the DB
+            trending_acqs_coll = dm.db.trending_acquisitions
+            query = {"timestamp": timestamp}
+            update_collection(trending_acqs_coll, query, df)
+
+    # Get metrics from tracking json
+    print(f"Checking tracking json file at {args.tracking_json}")
+    with open(args.tracking_json, "r") as f:
+        scenes = json.load(f)["features"]
+        for s in scenes:
+            p = s["properties"]
+            timestamp_str = os.path.basename(p["fid"]).split("_")[0][4:]
+            timestamp = dt.datetime.strptime(timestamp_str, "%Y%m%dt%H%M%S")
+            # Only process scenes in the argument time range
+            if start_date < timestamp < stop_date:
+                df = {
+                    "timestamp": timestamp,
+                    "aspect": p["Aspect (local surface aspect 0 to 360 degrees clockwise from N)"],
+                    "cosine_i": p["Cosine(i) (apparent local illumination factor based on DEM slope and aspect and to sun vector)"],
+                    "earth_sun_distance": p["Earth-sun distance (AU)"],
+                    "path_length": p["Path length (sensor-to-ground in meters)"],
+                    "slope": p["Slope (local surface slope as derived from DEM in degrees)"],
+                    "solar_phase": p["Solar phase (degrees between to-sensor and to-sun vectors in principal plane)"],
+                    "to_sensor_azimuth": p["To-sensor azimuth (0 to 360 degrees CW from N)"],
+                    "to_sensor_zenith": p["To-sensor zenith (0 to 90 degrees from zenith)"],
+                    "to_sun_azimuth": p["To-sun azimuth (0 to 360 degrees CW from N)"],
+                    "to_sun_zenith": p["To-sun zenith (0 to 90 degrees from zenith)"],
+                    "utc_time_decimal_hours": p["UTC Time (decimal hours for mid-line pixels)"]
+                }
+                if "Cloud + Cirrus Fraction" in p:
+                    df["cloud_plus_cirrus_fraction"] = p["Cloud + Cirrus Fraction"]
+                if "Cloud Fraction" in p:
+                    df["cloud_fraction"] = p["Cloud Fraction"]
+                if "Clouds & Buffer Fraction" in p:
+                    df["clouds_and_buffer_fraction"] = p["Clouds & Buffer Fraction"]
+                if "Screened Onboard Fraction" in p:
+                    df["screened_onboard_fraction"] = p["Screened Onboard Fraction"]
+                if "Total Cloud Fraction" in p:
+                    df["total_cloud_fraction"] = p["Total Cloud Fraction"]
+                if "Retrieved AOT Median" in p:
+                    df["retrieved_aot_median"] = p["Retrieved AOT Median"]
+                if "Retrieved Ele. Median" in p:
+                    df["retrieved_ele_median"] = p["Retrieved Ele. Median"]
+                if "Retrieved WV Median" in p:
+                    df["retrieved_wavelength_median"] = p["Retrieved WV Median"]
+
+                trending_acqs_coll = dm.db.trending_acquisitions
+                query = {"timestamp": timestamp}
+                update_collection(trending_acqs_coll, query, df)
 
 
 if __name__ == '__main__':
