e7ef458586431fcd924e13c2e2ce8e9caf408735
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2026-01-26 10:24:22 -0800

    Remove num_valid_lines check for l1b_mosaic and add task runtimes to daily metrics script.

--- emit_main/cron/stage_metrics.sh
+++ emit_main/cron/stage_metrics.sh
@@ -11,9 +11,13 @@ echo "$(date +"%F %T,%3N"): Executing stage_metrics.sh cron job for $1 environme
 source /local/miniforge3/etc/profile.d/conda.sh
 conda activate $2
 
+# Move current CSV files to bak folder
+mv /store/emit/$1/reports/grafana/*.csv /store/emit/$1/reports/grafana/bak/
+
 cd /store/emit/${1}/repos/emit-main/emit_main
 # Generate metrics
 srun -p emit -N 1 -c 64 --mem=320G --job-name=metrics --dependency=singleton python cron/compile_metrics.py --dates $(date -u -d "$3 days ago" "+%Y%m%d"),$(date -u "+%Y%m%d") >> /store/emit/$1/logs/cron_compile_metrics.log
-# Copy to staging location
-mv /store/emit/$1/reports/grafana/*.csv /store/emit/$1/reports/grafana/bak/
+# Export to CSV
 srun -p emit -N 1 -c 64 --mem=320G --job-name=metrics --dependency=singleton python cron/compile_metrics.py --dates $(date -u -d "$3 days ago" "+%Y%m%d"),$(date -u "+%Y%m%d") --export_to_dir /store/emit/$1/reports/grafana/
+# Generate task runtime CSV (just get last 7 days)
+python cron/slurm_metrics.py --dates $(date -u -d "7 days ago" "+%Y-%m-%d"),$(date -u "+%Y-%m-%d") --out_dir /store/emit/$1/reports/grafana/
