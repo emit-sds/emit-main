4154ea46bd5b3dd7b0eb08026b6d14ae23f2e823
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2026-09-11 14:00:46 -0700

    Modify checks to work with new DB structure.

--- emit_main/scripts/daily_checks.py
+++ emit_main/scripts/daily_checks.py
@@ -45,7 +45,7 @@ def main():
 
     # Check for DCIDs with missing frames
     query = {
-        "frames_status": "incomplete",
+        f"frames_status.{wm.config['prod_versions']['l1a']}": "incomplete",
         "associated_acquisitions": {"$exists": 0},
         "comments": {"$exists": 0}
     }
@@ -67,7 +67,7 @@ def main():
 
     # Check for orbits with missing BAD sto data
     query = {
-        "bad_status": "incomplete",
+        f"bad_status.{wm.config['prod_versions']['l1a']}": "incomplete",
         "associated_bad_netcdf": {"$exists": 0},
         "comments": {"$exists": 0}
     }
@@ -87,7 +87,7 @@ def main():
 
     # Check for orbits with missing raw
     query = {
-        "raw_status": "incomplete",
+        f"raw_status.{wm.config['prod_versions']['l1a']}": "incomplete",
         "num_scenes": {"$exists": 0},
         "comments": {"$exists": 0}
     }
@@ -108,8 +108,8 @@ def main():
 
     # Check for orbits with missing radiance
     query = {
-        "radiance_status": "incomplete",
-        "products.l1b": {"$exists": 0},
+        f"radiance_status.{wm.config['prod_versions']['l1b']}": "incomplete",
+        f"products.l1b.{wm.config['prod_versions']['l1b']}": {"$exists": 0},
         "comments": {"$exists": 0}
     }
     if start is not None:
@@ -130,8 +130,8 @@ def main():
 
     # Check for orbits that failed to geocorrect
     query = {
-        "radiance_status": "complete",
-        "products.l1b": {"$exists": 0},
+        f"radiance_status.{wm.config['prod_versions']['l1b']}": "complete",
+        f"products.l1b.{wm.config['prod_versions']['l1b']}": {"$exists": 0},
         "comments": {"$exists": 0}
     }
     if start is not None:
@@ -153,8 +153,8 @@ def main():
     query = {
         "submode": "science",
         "num_valid_lines": {"$gte": 320},
-        "products.l1a.raw.img_path": {"$exists": 1},
-        "products.l1b.rdn.img_path": {"$exists": 0}
+        f"products.l1a.{wm.config['prod_versions']['l1a']}.raw.img_path": {"$exists": 1},
+        f"products.l1b.{wm.config['prod_versions']['l1b']}.rdn.img_path": {"$exists": 0}
     }
     if start is not None:
         query["start_time"] = {"$gte": start, "$lt": stop}
